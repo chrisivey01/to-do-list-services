1dfb22d7358468c1df99552fde2db1b05cd250d4
chris <chrisivey01@gmail.com>
2019-02-09 16:56:08 -0600

    cron oops

--- src/main/java/com/list/todolist/controller/ListController.java
+++ src/main/java/com/list/todolist/controller/ListController.java
@@ -68,7 +68,7 @@ public class ListController {
         return "Text sent!";
     }
 
-    @Scheduled(cron = "*/30 * * * *")
+    @Scheduled(cron = "*/30 * * * * *")
     public void cron(){
         sendText();
     }
